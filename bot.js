5810f5c37d17c675cd0a394a4d14a0f8d823fa9f
Josh Klassen <joshuaklassen17@gmail.com>
2017-03-28 21:09:47 -0500

    Revert defaults. Add commented out disable_startup_messages config option

--- bot.js
+++ bot.js
@@ -35,7 +35,8 @@ var debug = require('debug')('botkit:main');
 
 // Create the Botkit controller, which controls all instances of the bot.
 var controller = Botkit.sparkbot({
-    debug: true,
+    // debug: true,
+    // disable_startup_messages: true,
     public_address: process.env.public_address,
     ciscospark_access_token: process.env.access_token,
     studio_token: process.env.studio_token, // get one from studio.botkit.ai to enable content management, stats, message console and more
